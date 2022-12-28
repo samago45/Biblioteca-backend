1bb3b5ab2fdc17ef30589ac814e22667305845e0
Sebastian Castro <sncastro20@gmail.com>
2022-12-28 15:03:34 -0300

    firs commint

---
+++ src/main/java/com/py/bibliotecajs/BibliotecaJsApplication.java
@@ -0,0 +1,13 @@
+package com.py.bibliotecajs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaJsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/py/bibliotecajs/BibliotecaJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.py.bibliotecajs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
